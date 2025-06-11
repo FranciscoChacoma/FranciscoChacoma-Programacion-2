81c789e7e11ff3824125b353c66eb21615542351
Fran <franciscochacoma11@mail.com>
2025-06-10 23:03:24 -0300

    Craftie un Menu en el Main y ahora a seguir con el tercer ejercicio D:

--- src/Funciones.java
+++ src/Funciones.java
@@ -1,9 +1,17 @@
 public class Funciones {
 
-
+    // Primer ejercicio
     public void mostrarSaludo()
     {
             System.out.print("Hola");
     }
+
+    // Segundo ejercicio
+    public void saludar(String nombre)
+    {
+        System.out.println("Hola" + nombre);
+    }
+
+    // Tercer Ejercicio
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
         Funciones Funciones = new Funciones();
 
-        Funciones.mostrarSaludo();
+        System.out.println("Menu");
+        System.out.println("1 - Saludo");
+        System.out.println("2 - Saludo y nombre");
+        int Opciones = scanner.nextInt();
+
+        switch (Opciones)
+        {
+            case 0:
+                Funciones.mostrarSaludo();
+                break;
+
+            case 1:
+                String nombre = "eteban";
+                Funciones.saludar(nombre);
+                break;
+            default:
+                System.out.println("Opcion no valdia");
+                break;
+        }
     }
 }
