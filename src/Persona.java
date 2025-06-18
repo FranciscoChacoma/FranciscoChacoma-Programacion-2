11b7635f90816bc779c591fe0a7bffca8f9340df
Fran <franciscochacoma11@mail.com>
2025-06-18 00:10:35 -0300

    Estuve practicando clases constructores setters y getters, asi que hice Persona.java e hice su clase y demas
    en el main no la llame para no molestar el menu q tengo pero lo probe y anda bien todo

--- src/Persona.java
+++ src/Persona.java
@@ -6,12 +6,95 @@ public class Persona {
     private String direccion;
     private String telefono;
 
-    public Persona(String nombre, String apellido, String DNI, int edad, String direccion, String telefono){
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.DNI = DNI;
-    this.edad = edad;
-    this.direccion = direccion;
-    this.telefono = telefono;
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido, String DNI, int edad, String direccion, String telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.DNI = DNI;
+        if (edad < 0)
+        {
+            System.out.println("Error: La Edad no puede ser menor a 0");
+            this.edad = 777777; // quiero poner edad invalida o algo
+        }
+        else
+        {
+            this.edad = edad;
+        }
+
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public String getNombre()
+    {
+     return nombre;
+    }
+
+    public void setnombre(String nombre)
+    {
+        if (nombre != null && !nombre.trim().isEmpty())
+        {
+            this.nombre = nombre;
+        }
+        else
+        {
+            System.out.println("Error: El Nombre no puede ser nulo");
+            this.nombre = "???";
+        }
+    }
+
+    public void setEdad(int edad)
+    {
+        if(edad < 0)
+        {
+            System.out.println("AAAAA");
+
+        }
+        else
+        {
+            this.edad = edad;
+        }
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
     }
 }
